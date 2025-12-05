cd3639dafc5c015e9dc55b09f65f2e2c812e1cb5
hongpeigoh <57928283+hongpeigoh@users.noreply.github.com>
2025-12-05 15:33:37 +0800

    chore: lint

--- ui/src/routes/__root.tsx
+++ ui/src/routes/__root.tsx
@@ -1,7 +1,5 @@
 import { Box } from "@mui/material";
-import { TanStackDevtools } from "@tanstack/react-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import Aos from "aos";
 import { useEffect } from "react";
 import { viewportStyle } from "../-styles/Layout";
